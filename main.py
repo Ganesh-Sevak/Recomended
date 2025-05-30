aeae683b49fe8dd6a28965b851d29c19d206e0e0
Ganesh-Sevak <gsevak585@gmail.com>
2025-05-30 16:41:34 -0500

    Access token done

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("hello world")
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post
+import json
+
+load_dotenv() #this loads the environemnt file
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    
+
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    print("Status code:", result.status_code)
+    token = json_result["access_token"]
+    return token
+
+token = get_token()
+
+
